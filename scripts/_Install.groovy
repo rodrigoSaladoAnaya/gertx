f8ea6ec84f7b95ba5b7073e79b6014055c2ca0b8
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2014-07-18 00:22:13 -0500

    issue #10

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,20 +1,27 @@
 ant.mkdir(dir: "${basedir}/web-app/js/libs")
 ant.copy(
-    file:"${pluginBasedir}/web-app/js/libs/sockjs.min.js", 
-    todir:"${basedir}/web-app/js/libs"
+        file: "${pluginBasedir}/web-app/js/libs/sockjs.min.js",
+        todir: "${basedir}/web-app/js/libs"
 )
 ant.copy(
-    file:"${pluginBasedir}/web-app/js/libs/vertxbus.js", 
-    todir:"${basedir}/web-app/js/libs"
+        file: "${pluginBasedir}/web-app/js/libs/vertxbus.js",
+        todir: "${basedir}/web-app/js/libs"
 )
 
 ant.mkdir(dir: "${basedir}/grails-app/gertx")
 ant.copy(
-    file:"${pluginBasedir}/grails-app/gertx/VerticleManager.groovy", 
-    todir:"${basedir}/grails-app/gertx", 
-    overwrite:true
+        file: "${pluginBasedir}/grails-app/gertx/VerticleManager.groovy",
+        todir: "${basedir}/grails-app/gertx",
+        overwrite: true
 )
 ant.copy(
-    file:"${pluginBasedir}/grails-app/gertx/EventBusBridge.groovy", 
-    todir:"${basedir}/grails-app/gertx"
+        file: "${pluginBasedir}/grails-app/gertx/EventBusBridge.groovy",
+        todir: "${basedir}/grails-app/gertx"
+)
+
+ant.mkdir(dir: "${basedir}/src/groovy/org/grails/gertx")
+ant.copy(
+        file: "${pluginBasedir}/src/groovy/org/grails/gertx/GertxSpecification.groovy",
+        todir: "${basedir}/src/groovy/org/grails/gertx",
+        overwrite: true
 )
