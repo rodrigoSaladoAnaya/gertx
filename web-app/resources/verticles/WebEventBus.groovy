f6aa1ab074f7c4e9dde6c0e7d659d0fd72fc030d
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2014-08-12 09:39:47 -0500

    Delete old VerticleManager and old WebEventBus

--- web-app/resources/verticles/WebEventBus.groovy
+++
@@ -1,9 +0,0 @@
-def server = vertx.createHttpServer()
-server.requestHandler { req ->
-}
-vertx.createSockJSServer(server).bridge(
-        prefix: '/eventbus',
-        [[:]],
-        [[:]]
-)
-server.listen(5540)
