86e4146f67574e4f3ce665eddac75d1e68fb326a
rodrigoSaladoAnaya <rodrigosaladoanaya@yahoo.com.mx>
2014-09-11 11:39:58 -0500

    issue #12

--- web-app/gertx/EventBusBridge.groovy
+++ web-app/gertx/EventBusBridge.groovy
@@ -1,9 +1,10 @@
 def server = vertx.createHttpServer()
+def config = container.config
 server.requestHandler { req ->
 }
 vertx.createSockJSServer(server).bridge(
-        prefix: '/eventbus',
+        prefix: "/${config.eventBusName?:'eventbus'}".toString(),
         [[:]],
         [[:]]
 )
-server.listen(5440)
+server.listen(config.eventBusPort?:5440)
